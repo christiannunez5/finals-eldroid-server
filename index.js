eb2c4385167d238484957a0eefd1160c2160aed5
christiannunez5 <christiannunez775@gmail.com>
2024-12-04 23:12:54 +0800

    increase limit

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
+app.use(express.json({ limit: "10mb" })); // Increase the limit to 10 MB
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
+
 app.use((req, res, next) => {
     console.log("METHOD: " + req.method);
     console.log("URL: " + req.url);
