1626c6c3eb989bb974134696e3ad3755f784c99c
christiannunez5 <christiannunez775@gmail.com>
2024-12-04 23:18:44 +0800

    zxczxczxc

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-app.use(express.json({ limit: "10mb" })); // Increase the limit to 10 MB
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ limit: "200mb", extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
